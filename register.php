cb8bc568bd59640450bb8b80ea47960333cf1d2b
aayushnarula <narulaaayush@gmail.com>
2018-09-09 22:54:16 +0530

    Add files via upload
    Login and Register in php basic.

---
+++ register.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Spotify!</title>
+</head>
+<body>
+	<div id = "inputContainer">
+		<form id="loginForm" action="register.php" method="POST">
+			<h2>Login to your account</h2>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type ="text" placeholder="Username" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginPassword" name="loginPassword" type ="Password" placeholder="Password" required>
+			</p>
+				<button type="submit" name="loginButton">LOG IN</button>
+		</form>
+		<br><br>
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create your free account</h2>
+
+			<p>
+				<label for="firstName">First Name</label>
+				<input id="firstName" name="firstName" type ="text" placeholder="Your FirstName" required>
+			</p>
+			<p>
+				<label for="lastName">Last Name</label>
+				<input id="lastName" name="lastName" type ="text" placeholder="Your LastName" required>
+			</p>
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type ="email" placeholder="Your Email" required>
+			</p>
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type ="email2" placeholder="Your Email" required>
+			</p>
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type ="password" placeholder="Your Password" required>
+			</p>
+			<p>
+				<label for="password2">Confirm Password</label>
+				<input id="password2" name="password2" type ="password" placeholder="Your Password" required>
+			</p>
+			<button type="submit" name="loginButton">Sign Up</button>	
+		</form>
+
+	</div>
+</body>
+</html>
